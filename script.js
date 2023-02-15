77f002ab7ccc01e782695f91da12e273b804e9a9
Borborem <uli77e7@yahoo.com.br>
2023-02-15 00:13:19 -0300

    Refatorando o script.js

--- script.js
+++ script.js
@@ -2,135 +2,6 @@ var clique = 0;
 var r2_isPlaying = false;
 var jogadas = [1,2,3,4,5,6,7,8,9];
 
-$(document).ready(function(){
-
-/*	$("#b00").click(function(){
-        
-        $(this).hide();
-		setTimeout(initiate,400)
-    });               */                
-	
-	$("#q1").click(function(){
-
-		if(!r2_isPlaying && jogadas.includes(1)){
-			clique++;
-			let index = jogadas.indexOf(1);
-			jogadas.splice(index,1);
-			var val="1";
-			drawFigure(val, clique);
-			r2_isPlaying = true;
-			setTimeout(r2plays,800);
-			
-		}
-		
-    });
-	
-	$("#q2").click(function(){
-		if(!r2_isPlaying && jogadas.includes(2)){
-			clique++;
-			let index = jogadas.indexOf(2);
-			jogadas.splice(index,1);
-			var val="2";
-			drawFigure(val, clique);
-			r2_isPlaying = true;
-			setTimeout(r2plays,800);
-			
-		}
-    });
-	
-	$("#q3").click(function(){
-		if(!r2_isPlaying && jogadas.includes(3)){
-			clique++;
-			let index = jogadas.indexOf(3);
-			jogadas.splice(index,1);
-			var val="3";
-			drawFigure(val, clique);
-			r2_isPlaying = true;
-			setTimeout(r2plays,800);
-			
-		}
-    });
-	
-	$("#q4").click(function(){
-		if(!r2_isPlaying && jogadas.includes(4)){
-			clique++;
-			let index = jogadas.indexOf(4);
-			jogadas.splice(index,1);
-			var val="4";
-			drawFigure(val, clique);
-			r2_isPlaying = true;
-			setTimeout(r2plays,800);
-			
-		}
-    });
-	
-	$("#q5").click(function(){
-		if(!r2_isPlaying && jogadas.includes(5)){
-			clique++;
-			let index = jogadas.indexOf(5);
-			jogadas.splice(index,1);
-			var val="5";
-			drawFigure(val, clique);
-			r2_isPlaying = true;
-			setTimeout(r2plays,800);
-			
-		}
-    });
-	
-	$("#q6").click(function(){
-		if(!r2_isPlaying && jogadas.includes(6)){
-			clique++;
-			let index = jogadas.indexOf(6);
-			jogadas.splice(index,1);
-			var val="6";
-			drawFigure(val, clique);
-			r2_isPlaying = true;
-			setTimeout(r2plays,800);
-			
-		}
-    });
-	
-	$("#q7").click(function(){
-			if(!r2_isPlaying && jogadas.includes(7)){
-			clique++;
-			let index = jogadas.indexOf(7);
-			jogadas.splice(index,1);
-			var val="7";
-			drawFigure(val, clique);
-			r2_isPlaying = true;
-			setTimeout(r2plays,800);
-			
-		}
-    });
-	
-	$("#q8").click(function(){
-		if(!r2_isPlaying && jogadas.includes(8)){
-			clique++;
-			let index = jogadas.indexOf(8);
-			jogadas.splice(index,1);
-			var val="8";
-			drawFigure(val, clique);
-			r2_isPlaying = true;
-			setTimeout(r2plays,800);
-			
-		}
-    });
-	
-	$("#q9").click(function(){
-		if(!r2_isPlaying && jogadas.includes(9)){
-			clique++;
-			let index = jogadas.indexOf(9);
-			jogadas.splice(index,1);
-			var val="9";
-			drawFigure(val, clique);
-			r2_isPlaying = true;
-			setTimeout(r2plays,800);
-			
-		}
-    });
-		
-});
-
 function r2plays(){
 	maxQuant = jogadas.length;
 	console.log(maxQuant);
@@ -159,4 +30,20 @@ function drawFigure(coord, clique){
         $(place).addClass("point");
     }
 
+}
+
+function ondeClicou(event){
+	qClicked = event.target.id; //q1
+	let qRef = parseInt(qClicked.slice(-1));
+	
+	if(!r2_isPlaying && jogadas.includes(qRef)){
+		clique++;
+		let index = jogadas.indexOf(qRef);
+		jogadas.splice(index,1);
+		var val = qRef.toString();
+		drawFigure(val, clique);
+		r2_isPlaying = true;
+		setTimeout(r2plays,800);
+		
+	}
 }
